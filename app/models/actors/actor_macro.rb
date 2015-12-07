4ca1a54e145f99e94291d3b7a8fafcdde368815f
rschumann <sebastian.schkudlara@gmail.com>
2015-12-07 13:40:10 +0100

    Move select options for actors to methods

--- app/models/actors/actor_macro.rb
+++ app/models/actors/actor_macro.rb
@@ -7,6 +7,10 @@ class ActorMacro < Actor
     OPERATIONAL_FIELDS[operational_field - 1]
   end
 
+  def operational_field_select
+    OPERATIONAL_FIELDS.map.with_index(1).to_a
+  end
+
   def empty_relations?
     parents.empty?
   end
