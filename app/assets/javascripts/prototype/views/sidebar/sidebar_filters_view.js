b2c6dd9ede5d22bfd322df9771b87dc298992db6
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-01-13 19:46:09 +0100

    Refactors the whole sidebar_filters_view to be less dependant from the DOM structure

--- app/assets/javascripts/prototype/views/sidebar/sidebar_filters_view.js
+++ app/assets/javascripts/prototype/views/sidebar/sidebar_filters_view.js
@@ -16,17 +16,18 @@
     el: '#sidebar-filters-view',
 
     events: {
-      'click .js-reset': 'resetForm',
-      'click .js-apply': 'applyFilters',
-      'change .toggle-button': 'toggleContent',
-      'change input[type=checkbox]:not(.toggle-button)': 'onCheckboxChange'
+      'click .js-reset': 'onResetFilters',
+      'click .js-apply': 'onApplyFilters',
+      'change .toggle-button': 'onExpandFilter',
+      'change input[type=checkbox]:not(.toggle-button)': 'onCheckboxChange',
+      'click .js-toggle-check': 'onClickToggleCheckButton'
     },
 
     initialize: function(options) {
       this.router = options.router;
       this.status = new Status();
       this.$inputFields = this.$el.find('.js-input');
-      this.syncHiddenInputsWithQueryParams();
+      this.syncInputsWithQueryParams();
       this.syncCheckboxesWithHiddenInputs();
       this.setListeners();
     },
@@ -45,41 +46,363 @@
       }
     },
 
-    /* Reset the form to its original state and sets the default query
-     * parameters in the URL */
-    resetForm: function(e) {
+    /* GETTERS */
+
+    /* From the element passed as argument, return the filter's root element
+     * which contains it (or null if can't find it)
+     * NOTE: expect a DOM element as parameter */
+    getFilterRootElem: function(childElem) {
+      return root.app.Helper.utils.getClosestParent(childElem, '.section');
+    },
+
+    /* Get the filter's name from it's DOM element/container */
+    getFilterName: function(filterRootElem) {
+      var selector = 'label';
+      return filterRootElem.querySelector(selector).textContent;
+    },
+
+    /* Return the content container of the selected filter */
+    getFilterContent: function(filterRootElem) {
+      var selector = '.js-content';
+      return filterRootElem.querySelector(selector);
+    },
+
+    /* Return the options for the filter whose root element is passed as
+     * argument */
+    getFilterCheckboxes: function(filterRootElem) {
+      var selector = '.js-content input[type="checkbox"]';
+      return filterRootElem.querySelectorAll(selector);
+    },
+
+    /* Return the checkbox matching the value passed as argument which belongs
+     * to the filter "named" in the arguments */
+    getFilterCheckboxByValue: function(filterRootElem, value) {
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+      var checkbox = _.filter(checkboxes, function(checkbox) {
+        return checkbox.value === value;
+      });
+
+      if(checkbox.length > 0) { return checkbox[0]; }
+      return null;
+    },
+
+    /* Return the button to toggle all the options of the filter whose root
+     * element is passed as argument */
+    getFilterToggleCheckButton: function(filterRootElem) {
+      var selector = '.js-toggle-check';
+      return filterRootElem.querySelector(selector);
+    },
+
+    /* Return the hidden button which summarizes the filter whose root element
+     * is passed as argument */
+    getFilterHiddenInput: function(filterRootElem) {
+      var selector = '.js-input';
+      return filterRootElem.querySelector(selector);
+    },
+
+    /* Return all the hidden inputs */
+    getAllHiddenInputs: function() {
+      var selector = '.js-input[disabled="disabled"]';
+      return this.el.querySelectorAll(selector);
+    },
+
+    /* Return the notification badge of the filter whose root element is passed
+     * as argument */
+    getFilterNotificationBadge: function(filterRootElem) {
+      var selector = '.js-badge';
+      return filterRootElem.querySelector(selector);
+    },
+
+    /* UTILITIES FOR THE CHECKBOXES */
+
+    /* Return all the checked checkboxes of the filter whose root element is
+     * passed as argument */
+    getFilterCheckedCheckboxes: function(filterRootElem) {
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+      return _.filter(checkboxes, function(checkbox) {
+        return checkbox.checked;
+      });
+    },
+
+    /* Return all the unchecked checkboxes of the filter whose root element is
+     * passed as argument */
+    getFilterUncheckedCheckboxes: function(filterRootElem) {
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+      var checkedCheckboxes = this.getFilterCheckedCheckboxes(filterRootElem);
+      return _.difference(checkboxes, checkedCheckboxes);
+    },
+
+    /* Check all the checkboxes of the filter whose root element is passed as
+     * argument */
+    checkFilterAllCheckboxes: function(filterRootElem) {
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+      _.each(checkboxes, function(checkbox) { checkbox.checked = true; });
+    },
+
+    /* Uncheck all the checkboxes of the filter whose root element is passed as
+     * argument */
+    uncheckFilterAllCheckboxes: function(filterRootElem) {
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+      _.each(checkboxes, function(checkbox) { checkbox.checked = false; });
+    },
+
+
+    /* Return all the inputs */
+    getAllInputs: function() {
+      var selector = '.js-input';
+      return this.el.querySelectorAll(selector);
+    },
+
+    /* Return the input which name is passed as argument */
+    getInputByName: function(name) {
+      var inputs = this.getAllInputs();
+      var input = _.filter(inputs, function(input) {
+        return input.name === name;
+      });
+
+      if(input.length > 0) { return input[0]; }
+      return null;
+    },
+
+    /* EVENT HANDLERS */
+
+    onExpandFilter: function(e) {
+      var filterRootElem = this.getFilterRootElem(e.currentTarget);
+      this.expandFilter(filterRootElem);
+    },
+
+    onCheckboxChange: function(e) {
+      var filterRootElem = this.getFilterRootElem(e.currentTarget);
+      var toggleCheckButton = this.getFilterToggleCheckButton(filterRootElem);
+
+      this.updateFilterNotificationBadge(filterRootElem);
+      this.updateFilterToggleCheckButton(filterRootElem);
+      this.syncFilterHiddenInputWithCheckboxes(filterRootElem);
+    },
+
+    onClickToggleCheckButton: function(e) {
       e.preventDefault();
 
+      var filterRootElem = this.getFilterRootElem(e.currentTarget);
+      var checkedCheckBoxes = this.getFilterCheckedCheckboxes(filterRootElem);
+
+      if(checkedCheckBoxes.length > 0) {
+        this.uncheckFilterAllCheckboxes(filterRootElem);
+      } else {
+        this.checkFilterAllCheckboxes(filterRootElem);
+      }
+
+      this.updateFilterToggleCheckButton(filterRootElem);
+      this.updateFilterNotificationBadge(filterRootElem);
+    },
+
+    onResetFilters: function(e) {
+      e.preventDefault();
+      this.resetFilters();
+    },
+
+    onApplyFilters: function(e) {
+      e.preventDefault();
+      this.applyFilters();
+    },
+
+    /* LOGIC */
+
+    /* Expand or contract a filter depending of its current state */
+    expandFilter: function(filterRootElem) {
+      var content = this.getFilterContent(filterRootElem);
+      var isContracted = content.getAttribute('aria-hidden');
+
+      content.setAttribute('aria-hidden', !isContracted);
+    },
+
+    /* Update the text of the button used to toggle the check of checkbox of the
+     * "current" filter according to their state*/
+    updateFilterToggleCheckButton: function(filterRootElem) {
+      var toggleCheckButton = this.getFilterToggleCheckButton(filterRootElem);
+      var checkedCheckBoxes = this.getFilterCheckedCheckboxes(filterRootElem);
+
+      /* Not all the filters have this element */
+      if(!toggleCheckButton) return;
+
+      if(checkedCheckBoxes.length > 0) {
+        toggleCheckButton.textContent = I18n.translate('front.uncheck_all');
+      } else {
+        toggleCheckButton.textContent = I18n.translate('front.check_all');
+      }
+    },
+
+    /* Update the text of all the toggle check buttons according to their
+     * filter's checkboxes */
+    updateAllToggleCheckButtons: function() {
+      var hiddenInputs = this.getAllHiddenInputs();
+
+      var filterRootElem;
+      for(var i = 0, j = hiddenInputs.length; i < j; i++) {
+        filterRootElem = this.getFilterRootElem(hiddenInputs[i]);
+        this.updateFilterToggleCheckButton(filterRootElem);
+      }
+    },
+
+    /* Update the text of the notification badget attached to the "current"
+     * filter according to the number of checked checkboxes */
+    updateFilterNotificationBadge: function(filterRootElem) {
+      var notificationBadge = this.getFilterNotificationBadge(filterRootElem);
+
+      /* Some filters don't have any badge */
+      if(!notificationBadge) return;
+
+      var uncheckedCheckboxes =
+        this.getFilterUncheckedCheckboxes(filterRootElem);
+      var uncheckedCheckboxesCount = uncheckedCheckboxes.length;
+
+      if(uncheckedCheckboxesCount > 0) {
+        var checkedCheckboxes = this.getFilterCheckedCheckboxes(filterRootElem);
+        var checkedCheckboxesCount = checkedCheckboxes.length;
+
+        notificationBadge.textContent = checkedCheckboxesCount;
+      } else {
+        notificationBadge.textContent = I18n.translate('front.all');
+      }
+    },
+
+    /* Update all the notification badges according to their filter's checkboxes
+     */
+    updateAllToggleNotificationBadges: function() {
+      var hiddenInputs = this.getAllHiddenInputs();
+
+      var filterRootElem;
+      for(var i = 0, j = hiddenInputs.length; i < j; i++) {
+        filterRootElem = this.getFilterRootElem(hiddenInputs[i]);
+        this.updateFilterNotificationBadge(filterRootElem);
+      }
+    },
+
+    /* Update the hidden input of the filter section to which it belongs by
+     * taking the checkboxes as reference */
+    syncFilterHiddenInputWithCheckboxes: function(filterRootElem) {
+      var hiddenInput = this.getFilterHiddenInput(filterRootElem);
+      var hiddenInputOptions = hiddenInput.querySelectorAll('option');
+
+      var checkboxes = this.getFilterCheckboxes(filterRootElem);
+
+      var checkbox;
+      _.each(hiddenInputOptions, function(option) {
+        checkbox = _.filter(checkboxes, function(checkbox) {
+          return root.app.Helper.utils.matches(checkbox,
+            '[value="' + option.value + '"]');
+        })[0];
+        option.selected = checkbox.checked;
+      });
+    },
+
+    /* Get the query params stored in the URL and apply their values to the
+     * inputs */
+    syncInputsWithQueryParams: function() {
+      var queryParams = this.router.getQueryParams();
+      var input;
+      _.each(queryParams, function(value, key) {
+        input = this.getInputByName(key);
+        if(!input) {
+          console.warn('Unable to sync the query param "' + key +
+            '" with its associated input');
+        } else {
+          /* In case we need to update a multi-option select input */
+          if(root.app.Helper.utils.matches(input, 'select')) {
+            /* We deselect all the options */
+            _.each(input.options,
+              function(option) { option.selected = false; });
+
+            var option;
+            if(!_.isArray(value)) { value = [ value ]; }
+
+            /* Then, for each value of the array "value", we select the option
+             * whose value matches it */
+            for(var i = 0, j = value.length; i < j; i++) {
+              option = input.querySelector('option[value="' +
+                value[i] + '"]');
+              if(option) {
+                option.selected = true;
+              } else if(value[i] !== '[]') {
+                console.warn('Unable to find the option "' + value[i] +
+                  '" in the input "' + key + '"');
+              }
+            }
+          }
+
+          /* In case the field is a standard input, we update its value */
+          else { input.value = value; }
+        }
+      }, this);
+    },
+
+    /* Apply the state of each hidden input to the associated checkboxes (the
+     * reverse action is done by default) */
+    syncCheckboxesWithHiddenInputs: function() {
+      var hiddenInputs = this.getAllHiddenInputs();
+
+      for(var i = 0, j = hiddenInputs.length; i < j; i++) {
+        var filterRootElem = this.getFilterRootElem(hiddenInputs[i]);
+        var options = hiddenInputs[i].options;
+
+        var checkbox;
+        for(var k = 0, l = options.length; k < l; k++) {
+          checkbox = this.getFilterCheckboxByValue(filterRootElem,
+              options[k].value);
+
+          if(checkbox) {
+            checkbox.checked = options[k].selected;
+          } else {
+            console.warn('Unable to restore the state of one option of the ' +
+              'filter ' + this.getFilterName(filterRootElem));
+          }
+        }
+
+        this.updateFilterNotificationBadge(filterRootElem);
+        this.updateFilterToggleCheckButton(filterRootElem);
+      }
+    },
+
+
+    /* Reset the form to its original state and sets the default query
+     * parameters in the URL */
+    resetFilters: function() {
+      var inputs = this.getAllInputs();
+
       var field;
-      for(var i = 0, j = this.$inputFields.length; i < j; i++) {
-        field = this.$inputFields[i];
+      for(var i = 0, j = inputs.length; i < j; i++) {
+        field = inputs[i];
+
         /* If the field is a select element, we select all the options */
         if(root.app.Helper.utils.matches(field, 'select')) {
           var options = field.options;
           for(var k = 0, l = options.length; k < l; k++) {
             options[k].selected = true;
           }
-        } else if(root.app.Helper.utils.matches(field, 'input[type="date"]')) {
-          var date = new Date();
-          field.value = [ ('0' + (date.getMonth() + 1)).slice(-2),
-            ('0' + date.getDate()).slice(-2), date.getFullYear() ].join('/');
         } else {
           field.value = '';
         }
       }
+
       this.syncCheckboxesWithHiddenInputs();
+      this.updateAllToggleCheckButtons();
+      this.updateallNotificationBadges();
+
       this.applyFilters();
     },
 
-    /* Save the form state into the status model */
-    applyFilters: function(e) {
-      if(e) { e.preventDefault(); }
-
+    /* Save the form state into the status model
+     * TODO: remove the console.warn once the form features are all implemented
+     */
+    applyFilters: function() {
       console.warn('The feature is only partially implemented');
 
+      var inputs = this.getAllInputs();
+
       var field, value, summary = {};
-      for(var i = 0, j = this.$inputFields.length; i < j; i++) {
-        field = this.$inputFields[i];
+      for(var i = 0, j = inputs.length; i < j; i++) {
+        field = inputs[i];
+
         /* If the field is a select element, we retrieve the value of the
          * selected option */
         if(root.app.Helper.utils.matches(field, 'select')) {
@@ -100,110 +423,6 @@
         }
       }
       this.router.setQueryParams(summary);
-    },
-
-    /* Toggle the aria-hidden attribute's value according to the visibility of
-     * the section */
-    toggleContent: function(e) {
-      $(e.currentTarget).parent().find('.js-content').attr('aria-hidden',
-        !e.currentTarget.checked);
-    },
-
-    /* Inside each section, the checkboxes are linked to a hidden select input
-     * which summarizes their values. This method updates the hidden input When
-     * a checkbox's value changed. */
-    updateHiddenInput: function(e) {
-      var hiddenInput = e.currentTarget.parentNode.parentNode
-        .querySelector('.js-input');
-      hiddenInput.querySelector('option[value="' +
-        e.currentTarget.value + '"]').selected = e.currentTarget.checked;
-    },
-
-    /* Apply the state of each hidden input to the associated checkboxes (the
-     * reverse action is done by default)
-     * NOTE: call updateNotificationBadgeFor to update the badge each time
-     * the checkboxes are updated as there's no way to know when it happens
-     * because the change event isn't triggered on the checkboxes when their
-     * status is modified in JS */
-    syncCheckboxesWithHiddenInputs: function() {
-      var hiddenInputs = this.$el.find('.js-input[disabled="disabled"]');
-      for(var i = 0, j = hiddenInputs.length; i < j; i++) {
-        var options = hiddenInputs[i].options;
-        var checkbox, selector;
-        for(var k = 0, l = options.length; k < l; k++) {
-          selector = 'input[type="checkbox"][value="' + options[k].value + '"]';
-          checkbox = hiddenInputs[i].parentNode.querySelector(selector);
-          checkbox.checked = options[k].selected;
-        }
-        this.updateNotificationBadgeFor(hiddenInputs[i].parentNode.parentNode);
-      }
-    },
-
-    /* Get the query params stored in the URL and apply their values to the
-     * hidden inputs */
-    syncHiddenInputsWithQueryParams: function() {
-      var queryParams = this.router.getQueryParams();
-      var hiddenInput;
-      _.each(queryParams, function(value, key) {
-        hiddenInput = this.$inputFields.filter('[name="' + key + '"]');
-        if(hiddenInput.length === 0) {
-          console.warn('Unable to sync the query param "' + key +
-            '" with its associated hidden input');
-        } else {
-          /* In case we need to update a multi-option select input */
-          if(root.app.Helper.utils.matches(hiddenInput[0], 'select')) {
-            /* We deselect all the options */
-            _.each(hiddenInput[0].options,
-                function(option) { option.selected = false; });
-
-            var option;
-            if(!_.isArray(value)) { value = [ value ]; }
-
-            /* Then, for each value of the array "value", we select the option
-             * whose value matches it */
-            for(var i = 0, j = value.length; i < j; i++) {
-              option = hiddenInput[0].querySelector('option[value="' +
-                value[i] + '"]');
-              if(option) {
-                option.selected = true;
-              } else if(value[i] !== '[]') {
-                console.warn('Unable to find the option "' + value[i] +
-                  '" in the hidden input "' + key + '"');
-              }
-            }
-          }
-
-          /* In case the field is a standard input, we update its value */
-          else { hiddenInput[0].value = value; }
-        }
-      }, this);
-    },
-
-    /* Handler for a checkbox's change (by checkbox we mean the actual filters
-     * options and not the hidden checkboxes used for the accordion) */
-    onCheckboxChange: function(e) {
-      this.updateNotificationBadgeFor(e.currentTarget.parentNode.parentNode.parentNode);
-      this.updateHiddenInput(e);
-    },
-
-    /* Update the number of checkboxes checked inside the notification badget of
-     * the filter's container
-     * NOTE: the element passed isn't a jQuery object */
-    updateNotificationBadgeFor: function(filterContainer) {
-      var badge = filterContainer.querySelector('.js-badge');
-
-      /* Some filters don't have any badge */
-      if(!badge) return;
-
-      var checkboxes = [].slice.call(filterContainer
-        .querySelectorAll('.js-content input[type="checkbox"]'));
-      var checkedCheckboxes = _.filter(checkboxes, function(checkbox) {
-        return checkbox.checked;
-      });
-      var uncheckedCheckboxes = _.difference(checkboxes, checkedCheckboxes);
-
-      badge.textContent = (uncheckedCheckboxes.length === 0) ?
-        I18n.translate('front.all') : checkedCheckboxes.length;
     }
 
   });
