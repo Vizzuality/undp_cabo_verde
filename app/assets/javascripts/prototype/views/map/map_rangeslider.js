f20447f1f19d25c3d419089a4ac0eeff57506e05
paulahaertel <paula.haertel@mailbox.org>
2016-01-19 11:57:17 +0100

    rangeslider wroking. TBD: data stuff

--- app/assets/javascripts/prototype/views/map/map_rangeslider.js
+++ app/assets/javascripts/prototype/views/map/map_rangeslider.js
@@ -25,9 +25,7 @@
       this.status = new Status();
 
       this._cacheVars();
-
       this._slider();
-
       this._cacheVars2();
 
     },
@@ -41,6 +39,7 @@
       this.ballCount = 0;
       this.posLeftHandle = 0;
       this.posRightHandle = 330; //set when first
+      this.steps = false;
     },
 
     _cacheVars2: function() {
@@ -53,13 +52,19 @@
 
     },
 
-    makeGrey: function() {
+    activateSteps: function() {
+      this.steps = true;
       this.leftHandle.style.background = "#cfcfcf";
       this.rightHandle.style.background = "#cfcfcf";
       this.$sliderRange[0].style.background = "#cfcfcf";
-
     },
 
+    deactivateSteps: function() {
+      this.steps = false;
+      this.leftHandle.style.background = "#175ca0";
+      this.rightHandle.style.background = "#175ca0";
+      this.$sliderRange[0].style.background = "#175ca0";
+    },
 
     create: function(htmlStr) {
       var frag = document.createDocumentFragment(),
@@ -75,6 +80,12 @@
       var range = this.$sliderRange[0];
       var ball = this.create('<div id="ball"></div>');
       range.insertBefore(ball, range.childNodes[0]);
+    },
+
+    deleteBall: function() {
+      var range = this.$sliderRange[0];
+      var ball = $('#ball')[0];
+      range.removeChild(ball);
     },
 
     calcStepWidth: function() {
@@ -98,17 +109,10 @@
     },
 
     step: function(e) {
-
       var range = this.$sliderRange[0];
-
-      this.makeGrey(); // makes handles and range background grey
-
-      this.calcStepRange(); //maximum number of steps
-      console.log('maxStepRange is ' + this.maxStepRange);
-
+      this.activateSteps(); // makes handles and range background grey
+      this.calcStepRange(); // calculates maximum number of steps currently set
       this.stepWidth = this.calcStepWidth(); //width of step in px
-
-      console.log("posLeftHandle: " + this.posLeftHandle);
 
       // if range has child:
       if(range.childNodes.length !== 0) {
@@ -131,7 +135,6 @@
 
             var newPositionInt = this.getNewPosition();
             var newPosition = newPositionInt.toString() + "px";
-            console.log('calculated new position: ' + newPosition);
 
           } else {
             var newPosition = "0px"; // security step in case the position is not set
@@ -141,12 +144,12 @@
           ball.style.left = newPosition;
           console.log('moved ball');
         }
+
       } else {
         //if no ball exists
         this.createBall();
         console.log('created ball');
       }
-
     },
 
     getNewPosition: function() {
@@ -161,48 +164,37 @@
       return newPosition;
     },
 
-    movedForward: function(newPosition, currPosition) {
-      var positionA = parseFloat(currPosition);
-      var positionB = parseFloat(newPosition);
-
-      if (positionA > positionB) {
-        return false;
-      } else {
-        return true;
-      }
-    },
-
     _slider: function() {
       var self = this;
-      var minYear = this.minYear;
-      var maxYear = this.maxYear;
 
-      //initial setup of labels
-      this.$leftLabel.text(minYear);
-      this.$rightLabel.text(maxYear);
+      //initial setup of labels: text and position
+      this.$leftLabel.text(this.minYear);
+      this.$rightLabel.text(this.maxYear);
       self.$leftLabel.css('left', this.posLeftHandle);     //TODO: position labels without pixel values
       self.$rightLabel.css('left', this.posRightHandle);  //TODO: position labels without pixel values
 
       this.$sliderSource.slider({
-        min: minYear,
-        max: maxYear,
+        min: this.minYear,
+        max: this.maxYear,
         range: true,
         step: 1,
-        values: [minYear, maxYear],
+        values: [this.minYear, this.maxYear],
         slide: function(event, ui) {
-          // console.log(event, ui);
           setTimeout(function() {
+            //reset
+            if ($('#ball')[0] !== undefined) {
+              self.deactivateSteps();
+              self.deleteBall();
+              self.ballCount = 0;
+            }
+
+            //sets label text
             self.$leftLabel.text(ui.values[0]);
             self.$rightLabel.text(ui.values[1]);
 
-            //position of handle: position of label
+            //sets position of handle (== position of label)
             self.posLeftHandle = $('.ui-slider-handle:first').position().left;
             self.posRightHandle = $('.ui-slider-handle:last').position().left;
-            if (self.ballCount > 0) {
-              //reset (TODO: make better)
-              $('#ball')[0].style.left = "0px";
-              self.ballCount = 0;
-            }
             self.$leftLabel.css('left', self.posLeftHandle);
             self.$rightLabel.css('left', self.posRightHandle);
           }, 0);
