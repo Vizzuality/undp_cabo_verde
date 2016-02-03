7779f7e026c655796bdf69df26a5b800193a3302
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-02-03 16:26:22 +0100

    Splits the map_view into several modules less dependant

---
+++ app/assets/javascripts/prototype/views/map/map_buttons.js
@@ -0,0 +1,50 @@
+(function(root) {
+
+  'use strict';
+
+  root.app = root.app || {};
+  root.app.View = root.app.View || {};
+
+  var Status = Backbone.Model.extend({
+    defaults: { sidebarVisible: true }
+  });
+
+  root.app.View.mapButtonsView = Backbone.View.extend({
+
+    el: '#map-buttons',
+
+    events: {
+      'change .js-relationships-checkbox': 'onRelationsToggle'
+    },
+
+    initialize: function() {
+      this.status = new Status();
+
+      this.relationsButton = this.el.querySelector('.js-relationships-checkbox');
+    },
+
+    onRelationsToggle: function(e) {
+      this.trigger('toggle:relations', { visible: e.currentTarget.checked });
+    },
+
+    /* Move the buttons to the right when the sidebar is hidden or to the left
+     * when it's expanded */
+    toggleButtonsPosition: function() {
+      this.el.classList.toggle('-slided', !this.status.get('sidebarVisible'));
+
+      if(!this.credits) {
+        this.credits = document.querySelector('.l-map .leaflet-control-attribution');
+      }
+      this.credits.classList.toggle('-slided',
+        !this.status.get('sidebarVisible'));
+    },
+
+    /* Toggle the relations buttons depending on the visible attribute from the
+     * options object passed as argument */
+    toggleRelationsButton: function(options) {
+      this.relationsButton.checked = options.visible;
+    }
+
+  });
+
+})(this);
---
+++ app/assets/javascripts/prototype/views/map/map_legend.js
@@ -0,0 +1,55 @@
+(function(root) {
+
+  'use strict';
+
+  root.app = root.app || {};
+  root.app.View = root.app.View || {};
+
+  var Status = Backbone.Model.extend({
+    defaults: { relationshipsVisible: true }
+  });
+
+  root.app.View.mapLegendView = Backbone.View.extend({
+
+    el: '#map-legend',
+
+    initialize: function(options) {
+      this.router = options.router;
+      this.status = new Status();
+
+      /* Cache for the relationships part of the legend */
+      this.$actorToActionLegend = this.$el.find('.js-actor-to-action');
+      this.$actorToActorLegend = this.$el.find('.js-actor-to-actor');
+      this.$actionToActionLegend = this.$el.find('.js-action-to-action');
+    },
+
+    /* Reduce the visible portion of the legend or not depending on if the
+     * relations are visible on the map */
+    toggleLegendPosition: function() {
+      this.el.classList.toggle('-reduced',
+        !this.status.get('relationshipsVisible'));
+    },
+
+    /* Dynamically hide a part of the relations depending on the active marker
+     * type: if a marker is passed as parameter (Leaflet object),
+     * consider it as the active marker, otherwise, take into account the marker
+     * attached to the current URL. If there's no active marker, reset the
+     * legend in its original state. */
+    updateLegendRelations: function(marker) {
+      if(marker) {
+        this.$actionToActionLegend.toggleClass('-disabled',
+          marker.options.type === 'actors');
+        this.$actorToActorLegend.toggleClass('-disabled',
+          marker.options.type === 'actions');
+      } else {
+        var route = this.router.getCurrentRoute();
+        this.$actionToActionLegend.toggleClass('-disabled',
+          route.name === 'actors');
+        this.$actorToActorLegend.toggleClass('-disabled',
+          route.name === 'actions');
+      }
+    }
+
+  });
+
+})(this);
