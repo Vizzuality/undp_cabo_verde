930d0a18aebdde2f5cebaf147b49b9eaa3a46f8e
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-02-09 17:16:13 +0100

    Fixes an issue where a related marker could be highlighted even if the relation wouldn' be displayed because of the time range

--- app/assets/javascripts/prototype/views/map/map_markers.js
+++ app/assets/javascripts/prototype/views/map/map_markers.js
@@ -440,11 +440,51 @@
       }
     },
 
+    /* Return the relations of the passed model */
+    extractRelations: function(model) {
+      var collections = [
+        {
+          relations: model.get('actors').parents,
+          type:       'actors',
+          hierarchy:  'parents'
+        },
+        {
+          relations: model.get('actors').children,
+          type:       'actors',
+          hierarchy:  'children'
+        },
+        {
+          relations: model.get('actions').parents,
+          type:       'actions',
+          hierarchy:  'parents'
+        },
+        {
+          relations: model.get('actions').children,
+          type:       'actions',
+          hierarchy:  'children'
+        }
+      ];
+
+      return _.reduce(_.map(collections, function(collection) {
+        return _.each(_.clone(collection.relations), function(relation) {
+          relation.type = collection.type;
+          relation.hierarchy = collection.hierarchy;
+        });
+      }), function(memo, collection) {
+        return _.union(memo, collection);
+      }, []);
+    },
+
     /* Highlight the leaflet markers passed as arguments and add to them a
      * special class so we know they're highlighted because they're linked with
      * the current clicked marker */
-    highlightRelatedMarkers: function(relatedMarkers) {
-      var domMarker;
+    highlightRelatedMarkers: function(marker, relatedMarkers) {
+      /* We compute the relations */
+      var model = marker.options.type === 'actors' ? this.actorModel :
+        this.actionModel;
+      var relations = this.extractRelations(model);
+
+      var domMarker, relation;
       _.each(relatedMarkers, function(relatedMarker) {
         domMarker = this.getMarkers(relatedMarker.options.type,
           relatedMarker.options.id, relatedMarker.options.locationId);
@@ -454,10 +494,48 @@
             relatedMarker.options.locationId ].join('/') +
             ' on the map');
         } else {
-          if(this.status.get('relationshipsVisible')) {
-            domMarker.classList.add('-active');
+          relation = _.findWhere(relations, {
+            type: relatedMarker.options.type,
+            id:   relatedMarker.options.id
+          });
+
+          if(!relation) {
+            console.warn('Unable to find the information about the relations ' +
+              'of /'+ relatedMarker.options.type + '/' +
+              relatedMarker.options.id);
+          } else {
+            var canHighlight = true;
+
+            /* In case the user filtered the dates in the search form, we make sure
+             * to display only the relations that exist in the range */
+            var queryParams = this.router.getQueryParams();
+            if(relation.info && (queryParams.start_date || queryParams.end_date)) {
+              if(relation.info.start_date &&
+                !root.app.Helper.utils.isDateBetween(relation.info.start_date, queryParams.start_date, queryParams.end_date) ||
+                relation.info.end_date &&
+                !root.app.Helper.utils.isDateBetween(relation.info.end_date, queryParams.start_date, queryParams.end_date)) {
+                canHighlight = false;
+              }
+            }
+
+            /* In case we're filtering a specific date with the timeline, we want
+             * to check if the relation can be displayed */
+            if(this.lastFilterDate && relation.info) {
+              if(relation.info.start_date &&
+                this.lastFilterDate < (new Date(relation.info.start_date)).getTime() ||
+                relation.info.end_date &&
+                this.lastFilterDate > (new Date(relation.info.end_date)).getTime()) {
+                canHighlight = false;
+              }
+            }
+
+            if(canHighlight) {
+              if(this.status.get('relationshipsVisible')) {
+                domMarker.classList.add('-active');
+              }
+              domMarker.classList.add('js-related-marker');
+            }
           }
-          domMarker.classList.add('js-related-marker');
         }
       }, this);
     },
